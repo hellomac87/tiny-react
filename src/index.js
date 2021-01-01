c6e3b779ec07f65c1debf1a03fb64cf9ded5b710
hellomac87 <hellomac87@gmail.com>
2021-01-01 23:04:00 +0900

    가상 돔은 이렇게 생기지 않았을까?

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("tiny react");
+import { createElement, render } from "./react";
+
+<div id="root">
+  <span>blabla</span>
+</div>;
+
+// 이렇게 생긴게 버추얼 돔 아닐까?
+/*
+{
+    tagName:"div",
+    props: {
+        id:"root",
+        className:'container',
+    },
+    children: [
+        tagName:"span",
+        props: {},
+        children: [
+            'blabla'
+        ]
+    ]
+}
+*/
